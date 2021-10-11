d1baa61ea6f4acf818cda7e0df0cfa2868b876f1
Kaminski <kaminski@Jakobs-MacBook-Pro-2.local>
2021-10-11 14:55:53 +0200

    test

---
+++ R_tutorial/import_data.R
@@ -0,0 +1,9 @@
+library(readr)
+p2 <- read_csv("~/ownCloud/Lehre/M28/Data/p2")
+
+p2[!duplicated(p2$ID),]
+
+write.csv(p2[!duplicated(p2$ID),],"~/ownCloud/Lehre/M28/Data/tablet2.csv")
+
+
+#test new
